82a2750b7c34059e65d93e287f71002eec6f6a94
leoFlutterNodeJs <leodevnew@gmail.com>
2022-08-22 14:01:57 -0300

    refactor: rename dist for distance

--- src/before/calc.ts
+++ src/before/calc.ts
@@ -3,7 +3,7 @@
 export function calculateRide(segments) {
 	let result = 0;
 	for (const segment of segments) {
-		if (segment.dist != null && segment.dist != undefined && typeof segment.dist === "number" && segment.dist > 0) {
+		if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
 			if (segment.ds != null && segment.ds != undefined && segment.ds instanceof Date && segment.ds.toString() !== "Invalid Date") {
 
 				// overnight
@@ -13,20 +13,20 @@ export function calculateRide(segments) {
 					// not sunday
 					if (segment.ds.getDay() !== 0) {
 
-						result += segment.dist * 3.90;
+						result += segment.distance * 3.90;
 						// sunday
 					} else {
-						result += segment.dist * 5;
+						result += segment.distance * 5;
 
 					}
 				} else {
 					// sunday
 					if (segment.ds.getDay() === 0) {
 
-						result += segment.dist * 2.9;
+						result += segment.distance * 2.9;
 
 					} else {
-						result += segment.dist * 2.10;
+						result += segment.distance * 2.10;
 
 					}
 				}
@@ -35,7 +35,7 @@ export function calculateRide(segments) {
 				return -2;
 			}
 		} else {
-			// console.log(dist);
+			// console.log(distance);
 
 			return -1;
 		}
